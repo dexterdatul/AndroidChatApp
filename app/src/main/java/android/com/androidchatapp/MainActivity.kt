1be87db7a914022e02998f6b3454b329cf1549d3
Dexter John Datul <ddatul@almadaclinic.com>
2017-12-11 21:39:45 +0300

    Navigation Drawer

--- app/src/main/java/android/com/androidchatapp/MainActivity.kt
+++ app/src/main/java/android/com/androidchatapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 
@@ -29,6 +30,18 @@ class MainActivity : AppCompatActivity(){
         }
     }
 
+
+    fun loginBtnNavClicked(view : View){
+
+    }
+
+    fun addChannelBtnClicked(view: View){
+
+    }
+
+    fun sendMessageBtnClicked(view: View){
+
+    }
 
 
 
