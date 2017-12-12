8b7a12be2b4e49672dfb8899c9453bebfc2a84dd
Dexter John Datul <ddatul@almadaclinic.com>
2017-12-12 17:43:21 +0300

    Functionality for random avatar change color Functionality for random avatar image

--- app/src/main/java/android/com/androidchatapp/CreateUserActivity.kt
+++ app/src/main/java/android/com/androidchatapp/CreateUserActivity.kt
@@ -1,25 +1,53 @@
 package android.com.androidchatapp
 
+import android.graphics.Color
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import kotlinx.android.synthetic.main.activity_create_user.*
+import java.util.*
 
 class CreateUserActivity : AppCompatActivity() {
+
+    var userAvatar = "profileDefault"
+    var avatarColor = "[0.5, 0.5, 0.5, 1]"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_user)
-    }
-
-    fun generateUserAvatar(view: View){
 
     }
 
-    fun generateColorClicked(view: View){
+    fun generateUserAvatar(view: View) {
+        val random = Random()
+        val color = random.nextInt(2)
+        val avatar = random.nextInt(28)
 
+        if (color == 0) {
+            userAvatar = "light$avatar"
+        } else {
+            userAvatar = "dark$avatar"
+        }
+        val resourceId = resources.getIdentifier(userAvatar, "drawable", packageName)
+        createAvatarImageView.setImageResource(resourceId)
     }
 
-    fun createUserClicked(view: View){
+    fun generateColorClicked(view: View) {
+        val random = Random()
+        var r = random.nextInt(255)
+        val g = random.nextInt(255)
+        val b = random.nextInt(255)
+
+        createAvatarImageView.setBackgroundColor(Color.rgb(r,g,b))
+
+        val savedR = r.toDouble() / 255
+        val savedG = r.toDouble() / 255
+        val savedB = r.toDouble() / 255
+
+        avatarColor = "[$savedR, $savedG, $savedB, 1 ]"
+    }
+
+    fun createUserClicked(view: View) {
 
     }
 
