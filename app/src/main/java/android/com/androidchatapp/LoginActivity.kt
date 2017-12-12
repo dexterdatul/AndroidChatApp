94fbffbf6fee81fc267537ba520a4d2047acd43d
Dexter John Datul <ddatul@almadaclinic.com>
2017-12-12 17:23:36 +0300

    Created Create User Activity

--- app/src/main/java/android/com/androidchatapp/LoginActivity.kt
+++ app/src/main/java/android/com/androidchatapp/LoginActivity.kt
@@ -1,5 +1,6 @@
 package android.com.androidchatapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
@@ -12,7 +13,8 @@ class LoginActivity : AppCompatActivity() {
     }
 
     fun loginCreateUserBtnClicked(view: View){
-
+        val createUserIntent = Intent(this, CreateUserActivity:: class.java)
+        startActivity(createUserIntent)
     }
 
     fun loginLoginBtnClicked(view: View){
